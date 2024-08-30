6cb33cdb5e4ced83151bf5ee5a33d9804c7015d0
SanamR14 <sanamrajaboopathi@gmail.com>
2024-08-30 08:17:09 +0100

    UI changes

--- src/register/register.component.ts
+++ src/register/register.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatInputModule } from '@angular/material/input';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [MatGridListModule, MatCardModule, MatInputModule, MatButtonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  form: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  submit() {
+    // if (this.form.valid) {
+    //   this.submitEM.emit(this.form.value);
+    // }
+    console.log(this.form.value);
+  }
 
 }
