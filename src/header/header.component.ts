73825b81d5700a2af62b860a0bcd8275452c2e60
SanamR14 <sanamrajaboopathi@gmail.com>
2024-09-13 10:05:28 +0100

    Responsive landing page

--- src/header/header.component.ts
+++ src/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider'
 import { Router } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatDividerModule],
+  imports: [MatDividerModule, MatButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
